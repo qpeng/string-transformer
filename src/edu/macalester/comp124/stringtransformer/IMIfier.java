f88a1093df904dc9b45cc03062bf0304de27d1a0
Brooke B <bboatman@macalester.edu>
2013-10-01 11:05:25 -0500

    Kathryn and Brooke ftw. Jk lmao. IM-ified for sure. You jelly bro?

---
+++ src/edu/macalester/comp124/stringtransformer/IMIfier.java
@@ -0,0 +1,41 @@
+package edu.macalester.comp124.stringtransformer;
+
+import java.util.Random;
+
+/**
+ * Created by Kathryn Hathaway and Brooke Boatman
+ */
+
+public class IMIfier extends IdentityTransformer{
+    @Override
+    public String transform(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[aeiou]", "");
+        Random random = new Random();
+        int insert = random.nextInt(4);
+        if (insert == 1) {
+            int word = random.nextInt(4);
+            if (word == 0) {
+                s += " lol";
+            }
+            if (word == 1) {
+                s += " lmao";
+            }
+            if (word == 2) {
+                s += " omg";
+            }
+            if (word == 3) {
+                s += " jk";
+            }
+            if (word == 4) {
+                s += " rofl";
+            }
+        }
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        return "IM-ified";
+    }
+}
