28b9d0b87e09e2192e9ba9ff546a59788a3848b1
qpeng <qpeng@macalester.edu>
2014-02-27 11:13:17 -0600

    Transformer (Qinghao Peng)

--- src/edu/macalester/comp124/stringtransformer/Reverser.java
+++ src/edu/macalester/comp124/stringtransformer/Reverser.java
@@ -2,7 +2,6 @@ package edu.macalester.comp124.stringtransformer;
 
 public class Reverser extends StringTransformer {
 
-    @Override
     public String transform(String s) {
         String rev= "";
         for (int i=0;i<s.length();i++) {
